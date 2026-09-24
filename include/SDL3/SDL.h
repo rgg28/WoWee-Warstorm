bf6db1b82f6a468882274c8c121552f639ae3b66
rgg28 <101537367+rgg28@users.noreply.github.com>
2026-09-24 20:06:01 -0300

    Update SDL.h

--- include/SDL3/SDL.h
+++ include/SDL3/SDL.h
@@ -2,5 +2,13 @@
 #define SDL3_BRIDGE_H
 
 typedef struct SDL_Window SDL_Window;
+typedef union SDL_Event SDL_Event;
+typedef struct SDL_Gamepad SDL_Gamepad;
+typedef int SDL_GamepadButton;
+typedef uint32_t SDL_JoystickID;
+typedef int SDL_Scancode;
+
+#define SDL_GAMEPAD_BUTTON_COUNT 15
+#define SDL_SCANCODE_COUNT 512
 
 #endif // SDL3_BRIDGE_H
